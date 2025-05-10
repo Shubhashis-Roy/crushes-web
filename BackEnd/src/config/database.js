0bc920f2e2c9d4d64fed47ddd0e21a67529de8a5
Shubhashis Roy <shubhashisroy360@gmail.com>
2025-05-10 11:55:12 +0530

    shift backend into diff repo

--- BackEnd/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://namasteNode:sroy@namastenode.kjfhv.mongodb.net/devTinder"
-  );
-};
-
-module.exports = connectDB;
