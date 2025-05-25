0abe23740dbeb122c2d9e629b8f804eef5d3ac9d
Shubhashis Roy <shubhashisroy360@gmail.com>
2025-05-26 00:21:44 +0530

    fix: make socket.io for production

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,16 +2,15 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "../utils/constants";
 
 // for local it not work in production
-export const createSocketConnection = () => {
-  return io(BASE_URL);
-};
+// export const createSocketConnection = () => {
+//   return io(BASE_URL);
+// };
 
 // FOR PRODUCTION
-// import io from "socket.io-client";
-// export const createSocketConnection = () => {
-//   if (location.hostname === "localhost") {
-//     return io(BASE_URL);
-//   } else {
-//     return io("/", { path: "/api/socket.io" });
-//   }
-// };
+export const createSocketConnection = () => {
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
+};
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-// export const BASE_URL =
-//   location.hostname === "localhost" ? "http://localhost:3001" : "/api";
-export const BASE_URL = "http://localhost:3001";
-// export const BASE_URL = "/api";
+//For Production
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3001" : "/api";
+
+// For: localhost
+// export const BASE_URL = "http://localhost:3001";
