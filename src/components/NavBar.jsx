b3f039e87bb08cb6fd9e2d786ac841d06a3ec996
Shubhashis Roy <shubhashisroy360@gmail.com>
2025-05-27 21:56:55 +0530

    fix: remove premium

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -82,14 +82,7 @@ const NavBar = () => {
                   Requests
                 </Link>
               </li>
-              <li>
-                <Link
-                  to="/premium"
-                  className="text-white px-4 py-2 rounded hover:bg-white/30"
-                >
-                  Premium
-                </Link>
-              </li>
+
               <li>
                 <button
                   onClick={handleLogout}
